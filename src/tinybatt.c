4b208fb39e99f60687dd4f9952f51dba290c8ffc
Sergey Frolov <dunkan.aidaho@gmail.com>
2015-04-07 16:07:28 +0300

    Add status indicators

--- src/tinybatt.c
+++ src/tinybatt.c
@@ -23,7 +23,7 @@
 #include "tinybatt.h"
 
 char *program_name;
-int debug = 1;  /**< debug flag */
+int debug = 0;  /**< debug flag */
 
 void print_version()
 /* Prints version information. */
@@ -54,10 +54,11 @@ void print_help()
 }
 
 int get_int_from_file(char *filepath) {
-	/* Reads first string from file with provided filepath,
-	   attempts to interpret it as integer and return to caller.
+	/*
+	  Reads first string from file with provided filepath,
+	  attempts to interpret it as integer and return to caller.
 
-	   \param filepath full file path to read from
+	  \param filepath full file path to read from
 	 */
 	FILE *fp;
 	int result;
@@ -66,6 +67,23 @@ int get_int_from_file(char *filepath) {
 	fclose(fp);
 	if (debug)
 		printf("%s\t%d\n", filepath, result);
+	return result;
+}
+
+char * get_first_line_from_file(char *filepath) {
+	/*
+	  Reads and returns first line from file in path.
+
+	  \param filepath full file path to read from
+	*/
+	size_t n = 0;
+	FILE *fp;
+	char *result = NULL;
+	fp = fopen(filepath, "rt");
+	getline(&result, &n, fp);
+	fclose(fp);
+	if (debug)
+		printf("%s\t%s\n", filepath, result);
 	return result;
 }
 
@@ -91,17 +109,36 @@ int main (int argc, char *argv[])
 		}
 	}
 
-	char *fpath;
-	int remaining_capacity, last_capacity, percentage;
+	char *fpath, *bat_status, output_part[32], output[255] = "";
+	int remaining_capacity, last_capacity, percentage, charging, warning;
+
 	asprintf(&fpath, "%s/%s", BATTERY_PATH, BATTERY_REMAINING_CAPACITY);
 	remaining_capacity = get_int_from_file(fpath) / CAPACITY_DIVIDER;
+
 	asprintf(&fpath, "%s/%s", BATTERY_PATH, BATTERY_LAST_CAPACITY);
 	last_capacity = get_int_from_file(fpath) / CAPACITY_DIVIDER;
+
 	/* I've tried a couple of userspace notifiers and it seems the general
 	 consensus is to round upwards. I guess pulling the cord and watching
-	 indicator goes from 100 to 99 twice as fast is frustrating. */
+	 how indicator goes from 100 to 99 twice as fast is frustrating. */
 	percentage = ceil(remaining_capacity / (last_capacity / 100));
 
-	printf("%d%%\n", percentage);
+	asprintf(&fpath, "%s/%s", BATTERY_PATH, BATTERY_STATUS);
+	bat_status = get_first_line_from_file(fpath);
+
+	charging = strstr(bat_status, CHARGING_STATUS) != NULL;
+	warning = percentage <= 10;
+
+	/* Construct output */
+	if (charging) {
+		strcat(output, CHARGING_STRING);
+	}
+	if (warning && !charging) {
+		strcat(output, WARNING_STRING);
+	}
+	sprintf(output_part, "%d%%", percentage);
+	strcat(output, output_part);
+
+	printf("%s\n", output);
 	exit(EXIT_SUCCESS);
 }
--- src/tinybatt.h
+++ src/tinybatt.h
@@ -4,6 +4,12 @@
 #define BATTERY_REMAINING_CAPACITY "energy_now"
 #define BATTERY_LAST_CAPACITY "energy_full"
 #define BATTERY_STATUS "status"
+// We will be looking at these substrings to determine status:
+#define CHARGING_STATUS "Charging"
+#define DISCHARGING_STATUS "Discharging"
+
+#define CHARGING_STRING "⚡"
+#define WARNING_STRING "⚠"
 
 #define CAPACITY_DIVIDER 1000
 #define CAPACITY_UNIT "mWh"
